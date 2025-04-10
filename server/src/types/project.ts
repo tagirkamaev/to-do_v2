521668b53308677add08506476eb3afaba402335
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-04-10 17:09:00 +0300

    Added owner as ObjectId and changed tasks to an array of ObjectId

--- server/src/types/project.ts
+++ server/src/types/project.ts
@@ -1,10 +1,13 @@
+import { ObjectId } from "mongodb";
+
 export interface IProject {
   title: string;
-  tasks: string[];
+  owner: ObjectId;
+  tasks: ObjectId[];
   createdAt: Date;
   updatedAt: Date;
 }
 
 export interface IProjectDocument extends IProject {
-  _id: string;
+  _id: ObjectId;
 }

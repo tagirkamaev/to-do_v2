dc2f863b5d5b25415b189b621c8059404ab65157
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-04-10 17:08:28 +0300

    Added project and owner properties

--- server/src/types/task.ts
+++ server/src/types/task.ts
@@ -1,11 +1,15 @@
+import { ObjectId } from "mongodb";
+
 export interface ITask {
   title: string;
   description?: string;
   completed: boolean;
+  project?: ObjectId;
+  owner: ObjectId;
   createdAt: Date;
   updatedAt: Date;
 }
 
 export interface ITaskDocument extends ITask {
-  _id: string;
+  _id: ObjectId;
 }

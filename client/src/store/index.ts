3061b2d0d299a9cb0ed9e07e147775771e980821
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-04-18 12:41:50 +0300

    Added projects and tasks reducers to Redux store configuration for improved state management of projects and tasks

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiReducer from "../features/ui/uiSlice";
 import authReducer from "../features/auth/authSlice";
+import projectsReducer from "../features/projects/projectsSlice";
+import tasksReducer from "../features/tasks/tasksSlice";
 
 // Store config
 export const store = configureStore({
   reducer: {
     ui: uiReducer,
     auth: authReducer,
+    projects: projectsReducer,
+    tasks: tasksReducer,
   },
 });
 
